524b8dd407cf35b500020908b3806141cb28a6aa
Princess Mae Sanchez <princessmae.sanchez04@gmail.com>
2026-05-31 22:41:54 +0800

    Add SplashCursorWrapper component

---
+++ components/SplashCursorWrapper.tsx
@@ -0,0 +1,6 @@
+"use client";
+import SplashCursor from "@/components/SplashCursor";
+
+export default function SplashCursorWrapper() {
+  return <SplashCursor TRANSPARENT={true} RAINBOW_MODE={false} COLOR="#c06080" />;
+}
